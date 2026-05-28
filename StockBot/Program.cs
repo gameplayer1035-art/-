30a34eae97d1c41bdd08eb94da4d94aa19e7c356
gameplayer1035-art <gameplayer1035@gmail.com>
2026-05-28 12:12:44 +0800

    Update Program.cs

--- StockBot/Program.cs
+++ StockBot/Program.cs
@@ -1,17 +1,23 @@
-using Microsoft.EntityFrameworkCore;
+using StockBot.Services; // 👈 關鍵！告訴程式去哪裡找那四個 Service
 using StockBot.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 注册服务
+// 註冊資料庫服務 (Railway 持久化路徑)
 builder.Services.AddDbContext<BotDbContext>(options =>
-    options.UseSqlite("Data Source=/app/data/stockbot.db")); // Railway 持久化路径
+    options.UseSqlite("Data Source=/app/data/stockbot.db")); 
 
+// 註冊你的所有自訂服務
 builder.Services.AddSingleton<AIService>();
 builder.Services.AddScoped<StockService>();
 builder.Services.AddScoped<MemoryService>();
 builder.Services.AddHostedService<DiscordHostedService>();
 
 var app = builder.Build();
+
 app.MapGet("/", () => "Stock Bot is running!");
+
 app.Run();
